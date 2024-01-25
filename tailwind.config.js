8bd0c4e016ee25add6ba53319ac0ddd58ca25922
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-01-25 13:43:07 +0530

    Initiated tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["*.html"],
   theme: {
     extend: {},
+    screens: {
+      sm: "300px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
